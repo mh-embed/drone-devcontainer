57448d7dec3263ec2466054cbed74372e04b74b2
Daniel Hou <danielhou315@gmail.com>
2023-02-17 15:33:52 -0500

    added git to image

--- drone_start.sh
+++ drone_start.sh
@@ -1,1 +1,6 @@
-sudo docker run --name drone-ros -it danielhouevr315/mhacker-drone-ros:latest --volume drone-ros-catkin-ws:/embed/catkin_ws/
+if [ $( sudo docker ps -a -f name=drone-ros | wc -l ) -eq 2 ]; then
+    sudo docker start drone-ros
+    sudo docker exec -it drone-ros bash
+else
+    sudo docker run --name drone-ros -it danielhouevr315/mhacker-drone-ros:latest --volume drone-ros-catkin-ws:/embed/
+fi
